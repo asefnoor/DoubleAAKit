cd4c280a8c671ead4903d7248a46af86f23cd845
Aqib Mumtaz <aqib.mumtaz@gmail.com>
2014-05-08 17:15:10 +0500

    Extensions

---
+++ UIViewController/UIViewController+Ext.m
@@ -0,0 +1,17 @@
+//
+//  UIViewController+Ext.m
+//  TT
+//
+//  Created by Aqib Mumtaz on 08/05/2014.
+//  Copyright (c) 2014 Aqib Mumtaz. All rights reserved.
+//
+
+#import "UIViewController+Ext.h"
+
+@implementation UIViewController (Ext)
+
+- (BOOL)isVisible {
+	return [self isViewLoaded] && self.view.window;
+}
+
+@end
